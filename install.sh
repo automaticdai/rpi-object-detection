6a4594ff3d4b2c9f2c7eebcb83af99c34715bc9e
Alessandro Pellegrino <alessandro.pellegrino@bioimed.it>
2024-11-02 08:34:56 +0100

    add installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
-#!/bin/bash  
+#!/bin/bash
 
-sudo apt-get install libopencv-dev -y
-sudo apt-get install libatlas-base-dev -y
+echo "Update packages"
+sudo apt-get update
 
-pip install -r requirements.txt
+echo "Installing libopencv-dev and libatlas-base-dev..."
+sudo apt-get install -y libopencv-dev libatlas-base-dev
+
+if ! command -v pip3 &> /dev/null; then
+    echo "pip3 not found. Installing pip3..."
+    sudo apt-get install -y python3-pip
+fi
+
+echo "Installing virtualenv, Pillow, numpy, scipy, matplotlib..."
+pip3 install virtualenv Pillow numpy scipy matplotlib
+
+echo "Installing opencv-python e opencv-contrib-python..."
+pip3 install opencv-python opencv-contrib-python
+
+echo "Installation of dependencies completed!"
